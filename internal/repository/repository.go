80860ba5e7a4849183834383cd9e7e97a481f662
lunarnuts <n.kuandyk@p-s.kz>
2024-08-08 11:26:52 +0500

    feat: add repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,19 +1,37 @@
 package repository
 
-import "github.com/lunarnuts/inboxsuite-test/internal/interfaces"
+import (
+	"context"
+	"github.com/lunarnuts/inboxsuite-test/internal/interfaces"
+	roadmap_repo "github.com/lunarnuts/inboxsuite-test/internal/repository/roadmap"
+	"github.com/lunarnuts/inboxsuite-test/pkg/postgres"
+	"log/slog"
+)
 
 var _ interfaces.Repository = (*Repository)(nil)
 
-func New() *Repository {
+func New(db *postgres.Gorm, log *slog.Logger) *Repository {
 	return &Repository{
-		roadmap: roadmap_repo.New(),
+		roadmap: roadmap_repo.New(log),
+		db:      db,
+		log:     log,
 	}
 }
 
 type Repository struct {
 	roadmap interfaces.RoadMapRepository
+	db      *postgres.Gorm
+	log     *slog.Logger
 }
 
-func (r Repository) RoadMap() interfaces.RoadMapRepository {
+func (r *Repository) RoadMap() interfaces.RoadMapRepository {
 	return r.roadmap
+}
+
+func (r *Repository) Conn() *postgres.Gorm {
+	return r.db
+}
+
+func (r *Repository) ConnWithContext(ctx context.Context) *postgres.Gorm {
+	return r.Conn().WithCtx(ctx)
 }
--- internal/repository/roadmap/roadmap.go
+++ internal/repository/roadmap/roadmap.go
@@ -1,12 +1,35 @@
 package roadmap_repo
 
-import "github.com/lunarnuts/inboxsuite-test/internal/interfaces"
+import (
+	"github.com/lunarnuts/inboxsuite-test/internal/entity"
+	"github.com/lunarnuts/inboxsuite-test/internal/interfaces"
+	"github.com/lunarnuts/inboxsuite-test/pkg/postgres"
+	"log/slog"
+)
 
 var _ interfaces.RoadMapRepository = (*Repository)(nil)
 
-func New() *Repository {
-	return &Repository{}
+func New(log *slog.Logger) *Repository {
+	return &Repository{
+		log: log,
+	}
 }
 
 type Repository struct {
+	log *slog.Logger
+}
+
+func (r *Repository) LoadCache(db *postgres.Gorm) (map[entity.ClassID]entity.Roadmap, error) {
+	const op = "Repository.LoadCache"
+	mp := make(map[entity.ClassID]entity.Roadmap)
+	var rows []*entity.Row
+	if err := db.Model(&entity.Row{}).Find(&rows).Error; err != nil {
+		r.log.Error(op, "error", err.Error())
+		return nil, err
+	}
+
+	for _, row := range rows {
+		mp[row.ClassID] = row.Roadmap
+	}
+	return mp, nil
 }
